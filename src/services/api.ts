6ca0918c09bb4e5734d7a720d4db6583f100b843
aishwaryaDel <aipathak@deloitte.com>
2025-11-21 12:01:18 +0530

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import { UseCase, CreateUseCaseDTO, UpdateUseCaseDTO } from '../types';
 
+//const API_BASE_URL = 'http://localhost:3001/api';
 const API_BASE_URL = 'http://localhost:3001/api';
 
 interface ApiResponse<T> {
